b71b45a2984cb6b7f2f6c09452471b3c6d858939
Matthew Hassell <hassell.matthew@gmail.com>
2017-03-14 17:16:59 -0400

    would like to clean up the whitespace

--- cpp_toys/find.cpp
+++ cpp_toys/find.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
-#include <regex>
 
 
 int main(int argc, char* argv[]){
@@ -17,43 +15,18 @@ int main(int argc, char* argv[]){
 	else{ // find the word (second argument) in the file (first argument)
 
 		const std::string in(argv[2]);
-		std::string reg("("+in+")");
-		std::regex e (reg);
-		std::cout<< reg << std::endl;
-	}
-
-}
-
-/*
-void split(const std::string &s, char delim, std::vector<std::string> &words) {
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        words.push_back(item);
-    }
-}
-
-int main(int argc, char* argv[]){
-
-	if(argc !=3 ){
-		std::cout << argc << std::endl;
-		std::cout << "Please input a file and a word to search for." << std::endl;
-		return 0;
-	}
-	else{ // find the word (second argument) in the file (first argument)
-
 		std::ifstream file(argv[1]);
-		std::vector<std::string> words;
 		std::string line;
-		while(std::getline(file,line))
-			split(line, ' ', words);
-			for(int i =0; i<words.size(); i++){
-				if(words[i] != " ")
-					std::cout << words[i] << '\n' << std::endl;
+		int line_num = 0;
+		while(std::getline(file,line)){
+			line_num++;
+			std::size_t found = line.find(in,0);
+			if(found!=std::string::npos){
+				std::cout << "Found on line " <<  line_num << "\n\n"<< std::endl;
+				std::cout << line << "\n\n" << std::endl;
 			}
 
+			line = "";
+		}
 	}
-
 }
-*/
