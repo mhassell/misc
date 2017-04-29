68ce4c4f7760c91d8166ae4000e41d046e7e9871
Matthew Hassell <hassell.matthew@gmail.com>
2017-04-29 08:51:00 -0400

    why is this working now??

--- gqrxTelnet.py
+++ gqrxTelnet.py
@@ -1,10 +1,11 @@
 import telnetlib
 
-HOST = '127.0.0.1'
+host = '127.0.0.1'
 port = 7356
 
-tn = telnetlib.Telnet(HOST, port=port)
-tn.open(HOST, port=port)
-tn.write('f')
-tn.read_eager()
+tn = telnetlib.Telnet(host, port)
+tn.write('f\n'.encode('ascii'))
+response = tn.read_some().decode('ascii').strip()
+print response
+tn.write('c\n'.encode('ascii'))
 tn.close()
