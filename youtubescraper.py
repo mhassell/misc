375dc7a7d35e83e3eafc6186a6307651302f2028
Matthew Hassell <hassell.matthew@gmail.com>
2017-07-21 20:47:42 -0400

    basic scraper working

--- youtubescraper.py
+++ youtubescraper.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 import os
 from pytube import YouTube
 
-# parse the page here
+nVideos = 5
 
+# parse the page here
 htmlParser = "lxml"
 url='https://www.youtube.com/playlist?list=UU9-y-6csu5WGm29I7JiwpnA'
 path = '/Users/matthewhassell/Desktop'
@@ -15,18 +16,34 @@ response=html.read()
 soup=BeautifulSoup(response, htmlParser)
 links = soup.find_all('a', attrs={'class':'pl-video-title-link'})
 
-links = links[0:5]   # get the 5 most recent videos
+links = links[0:nVideos]   # get the most recent videos
 
 # is this even new?
-lastVideoFile = open("LastVideos.txt", "rw")
+lastVideoFile = open("lastVideos.txt", "r+b")
 lastFive = lastVideoFile.read()
 
-for line in lastFive
+
+if lastFive == '':
+	for a in links:
+		lastVideoFile.write((a.get("href")))
+
+	lastVideoFile.close()
+else:
+	# work forward from the old list and backwards from the new list
+	for j in range(nVideos):
+		if links[nVideos-1-j].get("href") == lastFive[j]:
+			break
+	# these are the new ones		
+	links = links[0:nVideos-j]
+	lastFive = links + lastFive[j:]
 	
+	# take care of the file
+	lastVideoFile.seek(0)
+	lastVideoFile.write(lastFive)
+	lastVideoFile.close()
 
-(a.get("href"))
-
-yt = YouTube('https://www.youtube.com' + a.get("href"))
-video = yt.get('mp4')
-
-video.download('/Users/matthewhassell/Desktop')
+# now download the new ones
+for a in links:
+	yt = YouTube('https://www.youtube.com' + a.get("href"))
+	video = yt.get('mp4', '360p')
+	video.download('/Users/matthewhassell/Desktop')
